4b8aa52afa48c5315bf8a15709d60e6494c62a08
‘zhanxuewenliang <it_zdh@17530235750@163.com>
2021-11-03 20:45:56 +0800

    测试提交

---
+++ security-authorization-server/src/main/java/com/xwl/config/common/DemoCommon.java
@@ -0,0 +1,16 @@
+package com.xwl.config.common;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class DemoCommon {
+
+    private String name;
+    private String address;
+    private String sex;
+
+}
